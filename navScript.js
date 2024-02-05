76f4bbdfa4172316b05fe4f88200dbf230de63c1
Evan Martinez <emmplse821@gmail.com>
2024-02-05 14:09:17 -0800

    made the SVGs responsive via scroll

--- navScript.js
+++ navScript.js
@@ -1,5 +1,6 @@
 const navLeft = document.querySelector('.nav-left');
 const navRight = document.querySelector('.nav-right');
+const SVG = document.querySelectorAll('svg');
 
 
 let lastScrollTop = 0;
@@ -12,11 +13,13 @@ window.addEventListener('scroll', () => {
     console.log('scrolling down')
     navLeft.style.transform = 'translateX(-100%)';
     navRight.style.transform = 'translateX(100%)';
+    SVG.forEach(svg => {svg.style.transform = 'translateY(-90px)'});
   } else {
     console.log('scrolling up')
     // Scrolling up, reveal the sidebar
     navLeft.style.transform = 'translateX(0)';
     navRight.style.transform = 'translateX(0)';
+    SVG.forEach(svg => {svg.style.transform = 'translateY(0)'});
   }
 
   lastScrollTop = scrollTop;
--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const sectionSevenText = document.querySelector('#seven > div');
 var f = sectionSeven.getBoundingClientRect();
 const navLeft = document.querySelector('.nav-left');
 const navRight = document.querySelector('.nav-right');
+const SVG = document.querySelectorAll('svg');
 
 
 let freeze = false;
@@ -34,11 +35,13 @@ window.addEventListener('scroll', () => {
     console.log('scrolling down')
     navLeft.style.transform = 'translateX(-100%)';
     navRight.style.transform = 'translateX(100%)';
+    SVG.forEach(svg => {svg.style.transform = 'translateY(-90px)'});
   } else {
     console.log('scrolling up')
     // Scrolling up, reveal the sidebar
     navLeft.style.transform = 'translateX(0)';
     navRight.style.transform = 'translateX(0)';
+    SVG.forEach(svg => {svg.style.transform = 'translateY(0)'});
   }
 
   lastScrollTop = scrollTop;
